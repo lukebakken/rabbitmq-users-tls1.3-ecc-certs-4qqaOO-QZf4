b0bb67ddddbbe88e8472d5c54f727523e6ff81bf
Luke Bakken <luke@bakken.io>
2022-11-03 10:50:19 -0700

    Windows rmq script works

--- run-rabbitmq-server.ps1
+++ run-rabbitmq-server.ps1
@@ -8,26 +8,29 @@ Write-Host "[INFO] script directory: $PSScriptRoot"
 
 $rabbitmq_version='3.11.2'
 $rabbitmq_dir = Join-Path -Path $PSScriptRoot -ChildPath "rabbitmq_server-$rabbitmq_version"
-$rabbitmq_etc = Join-Path -Path $rabbitmq_dir -ChildPath 'etc' | Join-Path -ChildPath 'rabbitmq'
 $rabbitmq_sbin = Join-Path -Path $rabbitmq_dir -ChildPath 'sbin'
 $rabbitmq_download_url = "https://github.com/rabbitmq/rabbitmq-server/releases/download/v$rabbitmq_version/rabbitmq-server-windows-$rabbitmq_version.zip"
 $rabbitmq_zip_file = Join-Path -Path $PSScriptRoot -ChildPath "rabbitmq-server-windows-$rabbitmq_version.zip"
+$rabbitmq_plugins_cmd = Join-Path -Path $rabbitmq_sbin -ChildPath 'rabbitmq-plugins.bat'
+$rabbitmq_server_cmd = Join-Path -Path $rabbitmq_sbin -ChildPath 'rabbitmq-server.bat'
 
-# readonly rabbitmq_etc="$curdir/rabbitmq_server-$rabbitmq_version/etc/rabbitmq"
-# readonly rabbitmq_sbin="$curdir/rabbitmq_server-$rabbitmq_version/sbin"
-# sed "s|@@PWD@@|$PWD|" "$curdir/advanced.config.in" > "$rabbitmq_etc/advanced.config"
-# sed "s|@@PWD@@|$PWD|" "$curdir/rabbitmq.conf.in" > "$rabbitmq_etc/rabbitmq.conf"
-# if [ ! -d rabbitmq_server-3.11.2 ]
-# then
-#     curl -LO "https://github.com/rabbitmq/rabbitmq-server/releases/download/v$rabbitmq_version/rabbitmq-server-generic-unix-$rabbitmq_version.tar.xz"
-#     tar xf "rabbitmq-server-generic-unix-$rabbitmq_version.tar.xz"
-#     "$rabbitmq_sbin/rabbitmq-plugins" enable rabbitmq_management
-# fi
-# 
-# "$rabbitmq_sbin/rabbitmq-server"
+$rabbitmq_base = Join-Path -Path $PSScriptRoot -ChildPath 'rmq'
+$advanced_config_in = Join-Path -Path $PSScriptRoot -ChildPath 'advanced.config.in'
+$advanced_config_out = Join-Path -Path $rabbitmq_base -ChildPath 'advanced.config'
+$rabbitmq_conf_in = Join-Path -Path $PSScriptRoot -ChildPath 'rabbitmq.conf.in'
+$rabbitmq_conf_out = Join-Path -Path $rabbitmq_base -ChildPath 'rabbitmq.conf'
+
+$env:RABBITMQ_BASE = $rabbitmq_base
 
 if (!(Test-Path -Path $rabbitmq_dir))
 {
+    New-Item -Path $rabbitmq_base -ItemType Directory
     Invoke-WebRequest -Verbose -UseBasicParsing -Uri $rabbitmq_download_url -OutFile $rabbitmq_zip_file
     Expand-Archive -Path $rabbitmq_zip_file -DestinationPath $PSScriptRoot
+    & $rabbitmq_plugins_cmd enable rabbitmq_management
 }
+
+(Get-Content -Raw -Path $advanced_config_in) -Replace '@@PWD@@', $pwd | Set-Content -Path $advanced_config_out
+(Get-Content -Raw -Path $rabbitmq_conf_in) -Replace '@@PWD@@', $pwd | Set-Content -Path $rabbitmq_conf_out
+
+& $rabbitmq_server_cmd
