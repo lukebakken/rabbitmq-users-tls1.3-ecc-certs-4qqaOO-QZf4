ee1acfe6ac44cf54d8589bc8da79d142418cc961
Luke Bakken <luke@bakken.io>
2022-11-03 10:06:29 -0700

    RabbitMQ example is working

--- run-rabbitmq-server.sh
+++ run-rabbitmq-server.sh
@@ -4,9 +4,11 @@ set -eu
 
 readonly curdir="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
 readonly rabbitmq_version='3.11.2'
+readonly rabbitmq_etc="$curdir/rabbitmq_server-$rabbitmq_version/etc/rabbitmq"
 readonly rabbitmq_sbin="$curdir/rabbitmq_server-$rabbitmq_version/sbin"
 
-sed "s|@@PWD@@|$PWD|" "$curdir/rabbitmq.conf.in" > "$curdir/rabbitmq.conf"
+sed "s|@@PWD@@|$PWD|" "$curdir/advanced.config.in" > "$rabbitmq_etc/advanced.config"
+sed "s|@@PWD@@|$PWD|" "$curdir/rabbitmq.conf.in" > "$rabbitmq_etc/rabbitmq.conf"
 
 if [ ! -d rabbitmq_server-3.11.2 ]
 then
@@ -14,4 +16,5 @@ then
     tar xf "rabbitmq-server-generic-unix-$rabbitmq_version.tar.xz"
     "$rabbitmq_sbin/rabbitmq-plugins" enable rabbitmq_management
 fi
-RABBITMQ_ALLOW_INPUT=true RABBITMQ_CONFIG_FILE="$PWD/rabbitmq.conf" LOG=debug "$rabbitmq_sbin/rabbitmq-server"
+
+"$rabbitmq_sbin/rabbitmq-server"
