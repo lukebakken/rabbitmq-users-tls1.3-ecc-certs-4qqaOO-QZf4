1b4bc37a5e019cea2aff3de041b6fa8776d997ae
Luke Bakken <luke@bakken.io>
2022-11-04 14:58:43 -0700

    Error log level

--- run-tls-server.sh
+++ run-tls-server.sh
@@ -2,6 +2,6 @@
 # shellcheck disable=SC2155
 set -eu
 readonly curdir="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
-readonly logger_level="${1:-info}"
+readonly logger_level="${1:-error}"
 erlc +debug "$curdir/src/tls_server.erl"
 erl -noinput -kernel logger_level "$logger_level" -s tls_server start
