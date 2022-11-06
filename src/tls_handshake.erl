e86e099c2e62ac17c19e49a8de6f56f63484f2f8
Luke Bakken <luke@bakken.io>
2022-11-06 07:27:10 -0800

    Add test code from https://github.com/erlang/otp/issues/5255

---
+++ src/tls_handshake.erl
@@ -0,0 +1,17 @@
+-module(tls_handshake).
+
+-export([start/0]).
+
+start() ->
+    ssl:start(),
+    Opts = [
+            {cacertfile, "./certs/rootCA.pem"},
+            {certfile,   "./certs/cert.pem"},
+            {keyfile,    "./certs/key.pem"},
+            {verify, verify_peer},
+            {fail_if_no_peer_cert, false},
+            {log_level, debug}],
+    {ok, ListenSocket} = ssl:listen(9999, Opts),
+    {ok, TLSTransportSocket} = ssl:transport_accept(ListenSocket),
+    {ok, _} = ssl:handshake(TLSTransportSocket),
+    ok.
